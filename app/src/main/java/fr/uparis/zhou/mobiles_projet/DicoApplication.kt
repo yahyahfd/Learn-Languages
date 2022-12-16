27ede4061394b029186ef1d763470cbd067991ea
HAFID Yahya <hafidyahya1999@gmail.com>
2022-12-16 16:43:03 +0100

    Revert "Merge branch 'Debut' into 'master'"
    This reverts merge request !1

--- app/src/main/java/fr/uparis/zhou/mobiles_projet/DicoApplication.kt
+++
@@ -1,9 +0,0 @@
-package fr.uparis.zhou.mobiles_projet
-
-import android.app.Application
-
-class DicoApplication : Application() {
-    val database by lazy{
-        DicoBD.getDatabase(this)
-    }
-}
--- app/src/main/java/fr/uparis/zhou/mobiles_projet/MainActivity.kt
+++ app/src/main/java/fr/uparis/zhou/mobiles_projet/MainActivity.kt
@@ -1,40 +1,11 @@
 package fr.uparis.zhou.mobiles_projet
 
-import android.content.Intent
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import fr.uparis.zhou.mobiles_projet.databinding.ActivityMainBinding
-import java.io.File
-import java.io.PrintWriter
-
+import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding = ActivityMainBinding.inflate( layoutInflater )
-        setContentView(binding.root)
-
-        val file = File(filesDir, "dico")
-        if (file.length() == 0L) {
-            file.appendText("autre\n")
-            file.appendText("larousse\n")
-            file.appendText("reverso\n")
-            file.appendText("cambridge\n")
-        }
-
-        binding.ajoutMot.setOnClickListener{
-            val myIntent = Intent(this@MainActivity, AjoutMotActivity::class.java)
-            this@MainActivity.startActivity(myIntent)
-        }
-
-        binding.afficherBdd.setOnClickListener{
-            val myIntent = Intent(this@MainActivity, AfficherBddActivity::class.java)
-            this@MainActivity.startActivity(myIntent)
-        }
-
-        binding.chercher.setOnClickListener{
-            val myIntent = Intent(this@MainActivity, RechercheActivity::class.java)
-            this@MainActivity.startActivity(myIntent)
-        }
+        setContentView(R.layout.activity_main)
     }
 }
